2b76e515d17509d392165f6c7745588f6d7140eb
test <contact@developpeur-informatique.ma>
2022-12-12 16:13:27 +0100

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-footer dark padless>
+      <v-card
+        flat
+        tile
+        class="black white--text py-5 px-5 text-center"
+        width="100%"
+      >
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-card-text class="white--text pt-2">
+              <v-btn icon>
+                <v-icon color="#75E6DA">fa-solid fa-people-line</v-icon>
+              </v-btn>
+            </v-card-text>
+          </v-col>
+
+         
+          <v-col cols="12" sm="4">
+            <v-card-text class="white--text pt-2">
+              <v-btn icon>
+                <v-icon color="#75E6DA">fa-solid fa-people-line</v-icon>
+              </v-btn>
+            </v-card-text>
+          </v-col>
+
+          
+  
+          
+          
+
+          <v-col cols="12" sm="4">
+            <v-card-text class="pt-2">
+              <v-btn class="mx-1 white--text" icon>
+                <v-icon color="#A7121D">mdi-heart</v-icon>
+              </v-btn>
+              Created by Browny & Saif
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
+
+      <v-row
+      justify="center"
+      no-gutters
+    >
+     
+      <v-col
+        class="teal accent-4 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Browny & Saif</strong>
+      </v-col>
+    </v-row>
+    </v-footer>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style>
+  </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar app color="#111111" dark flat class="px-8">
+      <v-btn icon>
+        <v-icon color="#75E6DA" height="100px"
+    width="150px">fa-solid fa-people-line</v-icon>
+      </v-btn>
+  
+      <v-spacer></v-spacer>
+      <v-btn text @click="scroll('home')">Home</v-btn>
+      <v-btn text @click="scroll('about')">About</v-btn>
+      <v-btn text @click="scroll('contact')">Contact</v-btn>
+      <v-btn text @click="scroll('services')">Services</v-btn>
+      <v-btn text @click="scroll('contact')">Create Account</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ behavior: "smooth" });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
