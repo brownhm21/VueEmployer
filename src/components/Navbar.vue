661736e1ed34ece9e6000902af864b6e9ce38517
test <contact@developpeur-informatique.ma>
2022-12-15 16:21:49 +0100

    first Register commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
       </v-btn>
   
       <v-spacer></v-spacer>
-      <v-btn text @click="scroll('home')">Home</v-btn>
+      <v-btn text @click="$router.push('Home')">Home</v-btn>
       <v-btn text @click="scroll('about')">About</v-btn>
       <v-btn text @click="scroll('contact')">Contact</v-btn>
       <v-btn text @click="scroll('services')">Services</v-btn>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Dashboard from "../views/Dashboard.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
     component: HomeView
   },
@@ -14,15 +15,25 @@ const routes = [
   {
     path: '/Register',
     name: 'Register',
-    component: () => import(/* webpackChunkName: "about" */ '../components/Register.vue')
-  },{
+    component: () => import(/* webpackChunkName: "Register" */ '../components/Register.vue')
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requiresAuth: true
+    }
   }
+  
 ]
 
 const router = new VueRouter({
