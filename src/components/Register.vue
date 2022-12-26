2656e44197994a33674cb0fbba9fafba198f3344
test <contact@developpeur-informatique.ma>
2022-12-26 12:03:08 +0100

    first commit again and again

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,24 +1,115 @@
 <template>
-    <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
-        <Navbar />
-        <v-container  fluid>
-      <v-row >
-    
-        <v-col cols="12">
-            <v-card class="mx-auto rounded-xl" flat>
-            <v-img :src="require('../assets/4pic.jpg')" contain max-height="100%">
-                <div class="d-flex align-center auth-bg pt-16 col-lg-4 col ">
-                    <v-row class="rounded-xl">
-                        <div class="mx-auto col-sm-8 col-md-6 col-lg-12 col-12 ">
-                            <v-card class="v-card--flat v-sheet theme--light">
-                                <v-card-text>
-                                    <p class="text-2xl font-weight-semibold text--primary mb-2">Register 🚀</p>
-                                    <p class="mb-2">Make your app management easy and fun! </p>
-                                </v-card-text>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
+    <Navbar />
+      <v-container class="fill-height" fluid>
+        <v-row :align="center" justify="center">
+          <v-img :src="require('../assets/4pic.jpg')" contain max-height="100%">
+          <v-col cols="12" sm="8" md="8" class="w-25 p-3" style="height: 10%;
+          margin-top: 5%;
+          margin-left: 5%;">
+            <v-card class="elevation-12 rounded-xl">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Sign in to Employerella</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
 
-                                <v-card-text>
-                                  <v-form ref="form" @submit.prevent="submit" lazy-validation v-model="valid">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                        <v-form ref="form" @submit.prevent="submit" lazy-validation v-model="valid">
+                          
+
     <v-text-field
+    :label="labels.email"
+      v-model="form.email"
+      type="email"
+      :error-messages="errors.email"
+      :rules="email_Rules"
+      :disabled="loading"
+      @input="clearErrors('email')"
+            prepend-icon="mdi-mail"
+    ></v-text-field>
+
+    <v-text-field
+    :label="labels.password"
+      v-model="form.password"
+      :append-icon="passwordHidden ? 'visibility_off' : 'visibility'"
+      @click:append="() => (passwordHidden = !passwordHidden)"
+      :type="passwordHidden ? 'password' : 'text'"
+      :error-messages="errors.password"
+      :disabled="loading"
+      :rules="pwdRules"
+      hint="At least 6 characters"
+      @input="clearErrors('password')"
+            prepend-icon="mdi-lock"
+            
+    ></v-text-field>
+
+    
+
+                        </v-form>
+                        <h3 class="text-center mt-4">Forgot your password ?</h3>
+                      </v-card-text>
+                      <div class="text-center mt-3" style="padding: 10%;">
+                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Hello, Friend!</h1>
+                        <h5
+                          class="text-center"
+                        >Enter your personal details and start journay with us</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Welcome Back!</h1>
+                        <h5
+                          class="text-center"
+                        >To Keep connected with us please login with your personnel info</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Ensure your email for registration</h4>
+                        <v-form ref="form" @submit.prevent="submit" lazy-validation v-model="valid">
+                          <v-text-field
     :label="labels.name"
       v-model="form.name"
       :error-messages="errors.name"
@@ -55,45 +146,27 @@
     ></v-text-field>
 
     <v-text-field
-      :label="labels.password_confirmation"
-      v-model="form.password_confirmation"
+      :label="labels.confirmpassword"
+      v-model="form.confirmpassword"
       :type="passwordHidden ? 'password' : 'text'"
-      :error-messages="errors.password_confirmation"
+      :error-messages="errors.confirmpassword"
       :disabled="loading"
       :rules="password_ConfirmationRules"
       @input="clearErrors('password')"
             prepend-icon="mdi-lock"
             
     ></v-text-field>
+    <v-spacer></v-spacer>
+    <div class="text-center mt-n5"  style="padding: 5%; display: flex; justify-content: space-between;">
+                        <v-btn rounded color="teal accent-3" dark  type="submit" :loading="loading" :disabled="loading || !valid">SIGN UP</v-btn>
+                        <v-btn  text :disabled="loading" @click="step--" rounded color="grey darken-2" exact >Back to login</v-btn>
+                      </div>
 
-    <v-layout row class="mt-4 mx-0">
-      <v-spacer></v-spacer>
+                        </v-form>
+                      </v-card-text>
+                      
 
-      <v-btn
-        text
-        :disabled="loading"
-        :to="{ name: 'home' }"
-        color="grey darken-2"
-        exact
-      >
-        Back to login
-      </v-btn>
-
-      <v-btn
-        type="submit"
-        :loading="loading"
-        :disabled="loading || !valid"
-        color="primary"
-        class="ml-4"
-      >
-        Register
-      </v-btn>
-    </v-layout>
-  </v-form>
-                                       
-                                   
-                                </v-card-text>
-                                <v-divider></v-divider>
+                      <v-divider></v-divider>
             
                                 <v-card-text class="d-flex align-center justify-center flex-wrap mt-2">
                                     <span class="me-2">Already have an account?</span>
@@ -106,25 +179,18 @@
                                     <v-divider role="separator" aria-orientation="horizontal" class="theme--light"></v-divider>
                                 </v-card-text>
 
-                            </v-card>
 
-                        </div>
-
-
-                    </v-row>
-
-                </div>
-              
-
-            </v-img>
-          </v-card>
-        </v-col>
-      </v-row>
-      
-    </v-container>
-    <Footer />
-    </v-app>
-    
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-img>
+        </v-row>
+      </v-container>
+      <Footer />
+  </v-app>
 </template>
 
 <script>
@@ -150,7 +216,7 @@ validations: {
 
   data: () => ({
     passwordHidden: true,
-    
+    step: 1,
       pwdRules: [v => !!v || "Password required"],
       pwdConfirm: [
         v => !!v || "Confirm password",
@@ -159,9 +225,13 @@ validations: {
       name: null,
       email: null,
       password: null,
-      password_confirmation: null
+      confirmpassword: null
     }
   }),
+
+  props: {
+    source: String
+  },
 
  
 components: {
@@ -171,19 +241,22 @@ components: {
   methods: {
     submit() {
       if (this.$refs.form.validate()) {
-        
+        //this.loading = true;
         console.log('object',this.form);
-        /*axios.post(api.path('register'), this.form)
-          .then(res => {
-            this.$toast.success('You have been successfully registered!')
-            this.$emit('success', res.data)
+        axios.post('http://localhost:3000/api/user/register', this.form )
+          .then( (response) => {
+    console.log(response.data);
+    
+            this.$toast.success('You have been successfully registered!');
+            this.$emit('success', res.data);
+            
           })
           .catch(err => {
             this.handleErrors(err.response.data.errors)
           })
           .then(() => {
             this.loading = false
-          })*/
+          })
       }
     },
   },
@@ -220,7 +293,7 @@ components: {
     },
     password_ConfirmationRules() {
       return [
-        () => (this.form.password === this.form.password_confirmation) || 'password must match',
+        () => (this.form.password === this.form.confirmpassword) || 'password must match',
         v => !!v || 'Confirmation password is required'
       ];
     },
