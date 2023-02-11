66e09000f8e39dc054f07cad74691c04e962603d
test <contact@developpeur-informatique.ma>
2023-02-11 01:51:46 +0100

    new com

--- src/components/leaderboard.vue
+++ src/components/leaderboard.vue
@@ -3,15 +3,21 @@
     <v-data-table :headers="headers" :items="Employers" class="elevation-1" :items-per-page="5" :loading="loadTable"
       loading-text="Loading Leaderboard... Please wait">
       <template v-slot:item.avatar="{ item }">
-        <v-badge avatar bordered overlap >
-          <template v-slot:badge >
+        <v-badge avatar bordered overlap>
+          <template v-slot:badge>
             <v-avatar>
-              <v-img v-if="item.jobs.level === 'Senior'" src="http://strafrecht-online.org/stuff/trophy-gold.png"></v-img>
-              <v-img v-if="item.jobs.level === 'Junior'" src="http://strafrecht-online.org/stuff/trophy-silver.png"></v-img>
-              <v-img v-if="item.jobs.level === 'Trainee'" src="http://strafrecht-online.org/stuff/trophy-bronze.png"></v-img>
-               </v-avatar>
+              <v-img v-if="item.jobs.level === 'Senior'"
+                src="http://strafrecht-online.org/stuff/trophy-gold.png"></v-img>
+              <v-img v-if="item.jobs.level === 'Junior'"
+                src="http://strafrecht-online.org/stuff/trophy-silver.png"></v-img>
+              <v-img v-if="item.jobs.level === 'Trainee'"
+                src="http://strafrecht-online.org/stuff/trophy-bronze.png"></v-img>
+              <v-img v-if="item.jobs.level === 'Working Student'"
+                src="https://cdn-icons-png.flaticon.com/512/972/972228.png"></v-img>
+
+            </v-avatar>
           </template>
-          <v-avatar size="40">
+          <v-avatar size="50">
             <img :src="path + item.avatar" />
 
           </v-avatar>
@@ -49,28 +55,186 @@
         </v-chip>
       </template>
       <template v-slot:item.info="{ item, index }">
-        <v-btn icon color="primary" small @click.stop="$set(dialogNote, item.id, true)">
+        <v-btn icon color="primary" small @click.stop="$set(dialogNote, item._id, true)">
           <v-icon small>mdi-open-in-new</v-icon>
         </v-btn>
-        <v-btn icon color="primary" small @click.stop="$set(dialogNoteEdit, item.id, true)">
+        <v-btn icon color="primary" small @click.stop="$set(dialogNoteEdit, item._id, true)">
           <v-icon small>mdi-pencil</v-icon>
         </v-btn>
         <v-btn icon color="primary" small @click.stop="$set(dialogNoteDelete, item._id, true)">
           <v-icon small> mdi-delete</v-icon>
         </v-btn>
         <!--just test for 30/01/2023  -->
-        <v-dialog v-model="dialogNoteEdit[item.id]" max-width="290" :key="item.id" dark outlined rounded>
-          
+        <v-dialog v-model="dialogNoteEdit[item._id]" scrollable lazy :key="item._id" dark outlined rounded>
+          <template>
+            <v-row>
+              <v-form ref="form" class="d-flex flex-column justify-center gap-5">
+                <v-col cols="12">
+                  <v-card title="Account Details" elevation="12">
+                    <v-card-text class="d-flex ">
 
+                      <!-- 👉 Avatar -->
+                      <v-avatar 
+                       rounded="lg" size="100" class="me-6" color="primary" ><img :src="path + item.avatar" /></v-avatar>
+
+                      <!-- 👉 Upload Photo -->
+
+                      <div class="d-flex flex-wrap gap-1">
+                        <v-btn color="primary" @click="$refs.refInputEl?.click()">
+                          <v-icon icon="mdi-cloud-upload-outline" class="d-sm-none" />
+                          <span class="d-none d-sm-block">Upload new photo</span>
+                        </v-btn>
+
+                        <input ref="refInputEl" type="file" name="file" accept=".jpeg,.png,.jpg,GIF" hidden @input="">
+
+                        <v-btn type="reset" color="error" variant="tonal" @click="">
+                          <span class="d-none d-sm-block">Reset</span>
+                          <v-icon icon="mdi-refresh" class="d-sm-none" />
+                        </v-btn>
+
+                      </div>
+
+
+                    </v-card-text>
+
+                    <v-divider />
+                    <v-card-text>
+                      <v-row>
+                        <v-col cols="12">
+                        <div class="headline">Employer Infos</div>
+                      </v-col>
+                      </v-row>
+                      
+                      <v-row>
+
+                        <!-- 👉 First Name -->
+                        <v-col md="6" cols="12">
+                          <v-text-field label="First Name" v-model="item.Firstname" outlined
+                            prepend-inner-icon="mdi-account-arrow-right-outline" :rules="requiredRules" />
+
+                        </v-col>
+
+                        <!-- 👉 Last Name -->
+                        <v-col md="6" cols="12">
+                          <v-text-field label="Last Name" v-model="item.Lastname" outlined
+                            prepend-inner-icon="mdi-account-arrow-left-outline" :rules="requiredRules" />
+
+                        </v-col>
+
+                        <!-- 👉 Email -->
+                        <v-col cols="12" md="6">
+                          <v-text-field label="E-Mail" v-model="item.email" outlined :rules="emailRules"
+                            prepend-inner-icon="mdi-email-outline" />
+
+                        </v-col>
+
+                        <!-- 👉 Phone  -->
+                        <v-col cols="12" md="6">
+                          <v-text-field label="Phone Number" v-model="item.phoneNumber" outlined counter="10"
+                            :rules="numberRules" prepend-inner-icon="mdi-cellphone" />
+
+                        </v-col>
+
+                        <!-- 👉 Address -->
+                        <v-col cols="12" md="6">
+                          <v-text-field label="Address" v-model="item.address" outlined :rules="requiredRules"
+                            prepend-inner-icon="mdi-home-variant" />
+
+                        </v-col>
+
+                        <!-- 👉 city -->
+                        <v-col cols="12" md="6">
+                          <v-text-field label="City" v-model="item.city" outlined :rules="requiredRules"
+                            prepend-inner-icon="mdi-map-marker-multiple" />
+
+                        </v-col>
+
+
+
+                        <!-- 👉 Zip Code -->
+                        <v-col cols="12" md="6">
+                          <v-text-field label="Zip Code" v-model="item.zipcode" outlined counter="5" :rules="numberRules"
+                            prepend-inner-icon="mdi-map" />
+
+                        </v-col>
+
+
+
+
+                      </v-row>
+                      <v-row class="mb-0 mr-6">
+                        <v-col cols="12">
+                          <div class="headline">Jobs</div>
+                        </v-col>
+                      </v-row>
+                      <v-row class="rounded mb-2 mx-0" style="border: 1px solid lightgrey;">
+                        <v-col cols="12" md="3">
+                          <v-autocomplete :items="positions" label="Level" prepend-icon="mdi-domain"
+                            :rules="requiredRules" v-model="item.jobs.level" />
+                        </v-col>
+
+                        <v-col cols="12" md="4">
+                          <v-text-field label="Company Job" :rules="requiredRules" hint="The name is quite enough."
+                            prepend-icon="mdi-wallet-travel" v-model="item.jobs.companyjob"/>
+                        </v-col>
+
+                        <v-col cols="12" md="4">
+                          <v-autocomplete v-model="item.jobs.companyName" label="Company Name" prepend-icon="mdi-domain"
+                                :rules="requiredRules" item-value="_id" item-text="companyName" :items="companies" />
+                        </v-col>
+
+                        <v-col cols="12" md="4" v-if="item.jobs.level === 'Trainee'">
+                          <v-dialog ref="dialog" v-model="item.jobs.calenderModal" persistent width="290px">
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field v-model="item.jobs.dateRange" label="Employment period" prepend-icon="mdi-calendar"
+                                readonly v-bind="attrs" v-on="on" :rules="requiredRules"></v-text-field>
+                            </template>
+                            <v-date-picker v-model="item.jobs.dateRange" type="month" range>
+                              <v-spacer></v-spacer>
+                              <v-btn text color="primary" @click="(calenderModal = false); (dateRange = null);">
+                                Cancel
+                              </v-btn>
+                              <v-btn text color="primary" @click="validateDate(dateRange)">
+                                Save
+                              </v-btn>
+                            </v-date-picker>
+                          </v-dialog>
+                        </v-col>
+
+                      </v-row>
+                      <v-divider />
+                      <v-row class="mt-2">
+                        <v-col cols="12" class="d-flex flex-wrap gap-4">
+                          <v-btn>Save changes</v-btn>
+
+                          <v-btn color="secondary" variant="tonal" type="reset" @click.prevent="reset">
+                            Reset
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+
+
+                    </v-card-text>
+
+
+
+                  </v-card>
+                </v-col>
+              </v-form>
+
+
+            </v-row>
+
+
+          </template>
 
 
         </v-dialog>
         <!--  -->
-        <v-dialog v-model="dialogNoteDelete[item._id]" max-width="350" :key="item._id" dark outlined
-          rounded>
+        <v-dialog v-model="dialogNoteDelete[item._id]" max-width="450" :key="item._id" dark outlined rounded>
           <v-card>
             <v-card-title class="text-h5">
-              Do you want to delete <br> {{ item.Lastname }} 
+              Do you want to delete <br> {{ item.Lastname }}
             </v-card-title>
 
             <v-card-text>
@@ -80,20 +244,17 @@
             <v-card-actions>
               <v-spacer></v-spacer>
 
-              <v-btn color="green darken-1" text @click.stop="$set(dialogNoteDelete, item._id, false)" outlined
-                rounded>
+              <v-btn color="green darken-1" text @click.stop="$set(dialogNoteDelete, item._id, false)" outlined rounded>
                 Cancel
               </v-btn>
 
-              <v-btn color="red darken-1" text @click="deleteEmployer(item)" outlined
-                rounded>
+              <v-btn color="red darken-1" text @click="deleteEmployer(item)" outlined rounded>
                 Delete
               </v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
-        <v-dialog v-model="dialogNote[item.id]" scrollable lazy max-width="500" :key="item.id" dark outlined
-          rounded>
+        <v-dialog v-model="dialogNote[item._id]" scrollable lazy max-width="500" :key="item._id" dark outlined rounded>
           <v-card dark :img="require('@/assets/img/bgDrawer.jpg')">
             <v-card style="background-color: transparent;">
               <v-list-item three-line>
@@ -144,6 +305,18 @@
             </v-card> -->
 
             <v-card style="background-color: transparent;" class="pa-md-2 ">
+              <v-list-item v-if="item.jobs.level === 'Trainee'">
+                <v-list-item-avatar>
+                  <v-icon color="blue-grey darken-1">mdi-calendar-range</v-icon>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.jobs.startdate }} / {{ item.jobs.startdate }} </v-list-item-title>
+                </v-list-item-content>
+
+              </v-list-item>
+
+              <v-divider inset></v-divider>
 
               <v-list-item>
                 <v-list-item-avatar>
@@ -223,7 +396,7 @@
 
 
             <v-card-actions>
-              <v-btn flat @click.stop="$set(dialogNote, item.id, false)" outlined rounded text>Close</v-btn>
+              <v-btn flat @click.stop="$set(dialogNote, item._id, false)" outlined rounded text>Close</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -255,6 +428,8 @@ const statusColor = {
 
 };
 
+
+
 import axios from 'axios';
 import config from '../config';
 import Badge from "./Badge.vue";
@@ -270,6 +445,20 @@ export default {
   data() {
     return {
       /////////////////////
+      positions: [
+        "Trainee",
+        "Working Student",
+        "Intern",
+        "Junior",
+        "Senior",
+        "V-Level",
+        "C-Level"
+      ],
+      companies: [],
+      calenderModal: false,
+
+      Employers1: [],
+
       items: [],
       path: config.api_url,
       Employers: [],
@@ -322,6 +511,7 @@ export default {
       dialogNoteEdit: {},
       dialogNoteDelete: {},
 
+
       loadTable: false,
       headers: [
         {
@@ -343,7 +533,7 @@ export default {
         { text: "City", value: "city" },
         { text: "Level", value: "jobs.level" },
         { text: "Company Job", value: "jobs.companyjob" },
-        
+
 
         { text: "", value: "info", sortable: false },
       ],
@@ -352,62 +542,76 @@ export default {
     };
   },
   methods: {
-    EmployerDelete( index) {
-        axios.delete('http://localhost:3000/api/employer/delete-employer/'+item._id)
-        .then(response => this.Employers.splice(index, 1));
-      },
-      deleteItem() {
-        console.log('deleteItem', this.itemToDelete)
-        const index = this.Employers.indexOf(this.itemToDelete)
-        
-        /*
-        axios.delete(`https://api.airtable.com/v0/${airTableApp}/${airTableName}/${id}`,
-            { headers: { 
-                Authorization: "Bearer " + apiToken,
-                "Content-Type": "application/json"
-            }
-        }).then((response) => {
-            this.items.splice(index, 1)
-        })
-        */
-        
-        this.Employers.splice(index, 1)
-        
+    reset() {
+      this.$refs.form.reset();
     },
-     
-      deleteEmployer(item) {
+    EmployerDelete(index) {
+      axios.delete('http://localhost:3000/api/employer/delete-employer/' + item._id)
+        .then(response => this.Employers.splice(index, 1));
+    },
+    deleteItem() {
+      console.log('deleteItem', this.itemToDelete)
+      const index = this.Employers.indexOf(this.itemToDelete)
+
+      /*
+      axios.delete(`https://api.airtable.com/v0/${airTableApp}/${airTableName}/${id}`,
+          { headers: { 
+              Authorization: "Bearer " + apiToken,
+              "Content-Type": "application/json"
+          }
+      }).then((response) => {
+          this.items.splice(index, 1)
+      })
+      */
+
+      this.Employers.splice(index, 1)
+
+    },
+    ///////////////////////////
+    validateDate(dateRange) {
+      if (dateRange.length === 2) {
+        this.calenderModal = false;
+      }
+    },
+    ///////////////////////////working delete function
+    deleteEmployer(item) {
       axios.delete(`http://localhost:3000/api/employer/delete-employer/${item._id}`)
-      .then(response => 
-      
-      console.log(response ),
-      this.Employers.splice(this.Employers.indexOf(item),1),
+        .then(response =>
 
+          console.log(response),
+          this.Employers.splice(this.Employers.indexOf(item), 1),
 
-      
+        )
+        .catch(error => alert(error))
 
-      
-      
-      
-      )
-      .catch(error => alert(error)) 
-      
     },
     deleteItem(item) {
-            axios.delete('http://localhost:3000/api/employer/delete-employer/' + item.id)
-                .then(response => {
-                   const index = this.Employers.findIndex(item => item.id === id) // find the post index 
-                   if (~index) // if the post exists in array
-                     this.Employers.splice(index, 1) //delete the post
-                });
+      axios.delete('http://localhost:3000/api/employer/delete-employer/' + item.id)
+        .then(response => {
+          const index = this.Employers.findIndex(item => item.id === id) // find the post index 
+          if (~index) // if the post exists in array
+            this.Employers.splice(index, 1) //delete the post
+        });
 
-        },
-    deleteData: function(item, id) {
-      axios.delete('http://localhost:3000/api/employer/delete-employer' + item.id)
-      .then(response => {
-        this.item.splice(id, 1)
-        console.log(this.item);
-      });
     },
+    deleteData: function (item, id) {
+      axios.delete('http://localhost:3000/api/employer/delete-employer' + item.id)
+        .then(response => {
+          this.item.splice(id, 1)
+          console.log(this.item);
+        });
+    },
+
+    show: function () {
+      axios.get(`http://localhost:3000/api/employer/employerCompanyByuser/${this.userdata._id}`
+      )
+        .then(response => {
+          this.Employers1 = response.data;
+        })
+    },
+
+    
+
     // getColor1(companyjob) {
     //   if (companyjob = "Senior") return "red";
     //   else if (companyjob = "Junior") return "orange";
@@ -489,6 +693,24 @@ export default {
         //Then injecting the result to datatable parameters.
 
         this.Employers = response.data;
+
+      }).catch((error) => {
+        console.log(error)
+      });
+
+    axios.get(`http://localhost:3000/api/company/companyUserr/${this.userdata._id}`)
+      .then(response => {
+        this.companies = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    axios.get(`http://localhost:3000/api/employer/employerCompanyByuser/${this.userdata._id}`)
+      .then((response) => {
+        //Then injecting the result to datatable parameters.
+
+        this.Employers1 = response.data;
 
       }).catch((error) => {
         console.log(error)
---
+++ src/views/LoginView.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-app>
+      <navigation :color="primary"  />
+      <login />
+      
+      <v-main >
+    
+       <div class="svg-border-waves text-white pt-20">
+        <v-img src="~@/assets/img/borderWavesBlue.svg"/>
+      </div>
+        </v-main>
+        
+      
+      <foote />
+    </v-app>
+  </template>
+  <style scoped>
+  
+  </style>
+  
+  <script>
+  import navigation from "@/components/Navigation.vue";
+  import foote from "@/components/Footer.vue";
+  import login from "@/components/LoginAll.vue";
+
+
+
+ 
+  export default {
+    name: "LoginView",
+
+   
+    components: {
+      navigation,
+      foote,
+      login
+      
+    },
+    data: () => ({
+      fab: null,
+
+      color: "",
+      flat: null,
+      ///////////////////////
+      drawer: null,
+      snackbar: false,
+      snackbarText: '',
+    
+    error: '',
+    step: 1,
+    
+    
+    }),
+    props: {
+    source: String
+  },
+    created() {
+      const top = window.pageYOffset || 0;
+      if (top <= 60) {
+        this.color = "transparent";
+        this.flat = true;
+      }
+    },
+    watch: {
+      fab(value) {
+        if (value) {
+          this.color = "secondary";
+          this.flat = false;
+        } else {
+          this.color = "transparent";
+          this.flat = true;
+        }
+      },
+    },
+  
+  };
+  </script>
+
+<style scoped>
+.v-main {
+  background-image: url("~@/assets/img/login.png");
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+<style lang="scss">
+@use "../@core/scss/pages/page-auth.scss";
+</style>
