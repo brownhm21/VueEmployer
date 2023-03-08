f190f03d1afd454ab17923cd5d0fdd68816f3092
test <contact@developpeur-informatique.ma>
2023-03-08 16:43:06 +0100

    adding download vue

---
+++ src/components/LandingContact.vue
@@ -0,0 +1,157 @@
+<template>
+  <main>
+    <section id="hero">
+        <v-row>
+            <div class="col-lg-7 col-md-12 col-sm-12 col-12 none contact-container">
+                <!-- <span class="contact-text">
+                    <h1>
+                        LA COMIDA ES 
+                    <span class="background-yellow">
+                        NUESTRO ARTE
+                    </span>
+                    </h1>
+                    </span> -->
+                <img alt="contact" src="../assets/img/contact.svg"  height="100%" width="100%">
+            </div>
+
+            
+            <div class="col-lg-5 col-md-12 col-sm-12 col-12 none background">
+                    <span class="text">
+                        <h1>Who Are Employerella?</h1>
+                        <p>
+                            Elit irure ad nulla id elit laborum nostrud mollit irure.
+                            Velit reprehenderit sunt nulla enim aliquip duis tempor est culpa fugiat consequat culpa consectetur Lorem.
+                            Reprehenderit dolore culpa irure eiusmod minim occaecat et id minim ullamco.
+                        </p>
+                        <a href="#">Contact <i class="fa fa-arrow-right flechaIcon"/></a>
+                        </span>
+            </div>
+
+            
+        </v-row>
+        <div class="svg-border-waves text-white">
+          <v-img src="@/assets/img/borderWaves.svg" />
+        </div>
+       </section>
+  </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        name: 'LandingContact',
+        };
+    },
+}
+</script>
+
+<style lang="less" scoped>
+   // colors
+   @black: #000000;
+    
+
+    .none {
+        padding: 0px;
+        margin: 0px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        
+    }
+    .contact-container {
+        display: block;
+    } 
+    img {
+        vertical-align: middle;
+    }
+    .background{
+        position: relative;
+        display: inline-block;
+        background: url('../assets/img/contactDecorator4.svg');
+        background-color: none;
+        min-height: 351px;
+        background-position: right -120px top;
+    }
+    .contact-text {
+        position: absolute;
+        bottom: 60px;
+        right: 40px;
+        color: white;
+        text-align: end;
+        width: 535px;
+        h1 {
+            font-family: 'Druk Text Wide';
+            font-size: 50px;
+            line-height: 50px;
+        }
+        .background-yellow {
+            color: #ffd600;
+        }
+        @media (max-width: 600px){
+            bottom: 30px;
+            right: 20px;
+            width: 100%;
+        }
+        @media (max-width: 450px){
+            bottom: 15px;
+            right: 10px;
+            h1 {
+                font-size: 30px;
+            }
+        }
+    }
+
+    #hero {
+    z-index: 0;
+  }
+    .text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 450px;
+        
+        h1, a {
+            font-family: 'Syne';
+            font-size: 22px;
+            line-height: 26px;
+            color: @black;
+        }
+        h1 {
+            margin-bottom: 27px;
+        }
+        p {
+            font-family: 'Open Sans';
+            font-size: 18px;
+            line-height: 25px;
+            color: rgba(0, 0, 0, 0.4);
+            margin-bottom: 54px;
+        }
+        @media (min-width: 1367px){
+            width: 570px;
+        }
+        @media (max-width: 1100px){
+            width: 570px;
+        }
+        @media (max-width: 992px){
+            width: 570px;
+        }
+        @media (max-width: 600px){
+            width: 100%;
+            padding: 0 0px;
+        }
+        
+    }
+</style>
+<style>
+section{
+    position: relative;
+    background-color : transparent;
+}
+#hero {
+    z-index: 0;
+  }
+
+</style>
---
+++ src/components/SimplifyBlock.vue
@@ -0,0 +1,77 @@
+<template>
+  <!-- <div class="container">
+    <h2>Simplify how your team works today.</h2>
+    <div class="btn"><span>Get Started</span></div>
+  </div> -->
+  <main>
+    <section class="container">
+    <h2>Simplify how your team works today.</h2>
+    <div class="btn"><span>Get Started</span></div>
+  </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'SimplifyBlock'
+}
+</script>
+
+<style scoped>
+
+  .container {
+    background: url(../assets/img/bg-simplify-section-mobile.svg) no-repeat center left, var(--primary-red);
+    height: 50vh;
+    width: 100%;
+    padding-inline: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-block: 4rem;
+  }
+
+  h2 {
+    color: var(--neutral-light-gray);
+    text-align: center;
+    font-size: 2.5rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .btn {
+    color: var(--primary-red);
+    background: var(--neutral-light-gray);
+    font-weight: var(--fw-bold);
+    margin-inline: auto;
+  }
+
+  @media (min-width: 1440px) {
+    .container {
+      background: url(../assets/img/bg-simplify-section-mobile.svg) no-repeat bottom left 15rem, var(--primary-red);
+      background-size: cover;
+      padding-inline: 8rem;
+      height: 25vh;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+
+    h2 {
+      font-size: 2.3rem;
+      flex-basis: 40%;
+      text-align: start;
+    }
+
+    .btn {
+      margin-inline: unset;
+      flex-basis: 15%;
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1440px) {
+    .container {
+      height: 35vh; 
+    }
+  }
+</style>
